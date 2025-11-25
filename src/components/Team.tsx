5837c581a066b58f01c4f079718bfa83de68b26a
ArtifyMedia <farrukh955@gmail.com>
2025-11-26 04:15:07 +0500

    Updated Team.tsx

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -3,7 +3,7 @@ export function Team() {
     {
       name: 'Moiz Khan',
       title: 'Director of AI',
-      image: '/1764107555382(1).jpg',
+      image: '/Moiz Picture.jpg',
       bio: "I'm Moiz - I can't sort my own life out, but I can turn your business into a high-efficiency, revenue-producing machine.",
       description: (
         <>
@@ -15,7 +15,7 @@ export function Team() {
     {
       name: 'Haad Alvi',
       title: 'Director of Marketing',
-      image: '/1764107033769.jpg',
+      image: '/Haad Picture.jpg',
       bio: "I misplace my own charger daily, but I can turn your entire funnel into a predictable revenue machine.",
       description: (
         <>
