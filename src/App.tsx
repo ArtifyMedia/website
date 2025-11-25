f33326a24f54fa865f260edaf42cf3acac9bb9ab
ArtifyMedia <farrukh955@gmail.com>
2025-11-26 02:14:42 +0500

    Added package.json

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="container">
+      <header>
+        <h1>Artify Media</h1>
+      </header>
+      <main>
+        <p>Welcome to Artify Media</p>
+      </main>
+    </div>
+  )
+}
+
+export default App
